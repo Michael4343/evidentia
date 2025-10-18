1d89fe6d286e539a191d5f46f635873b3f3d4436
Michael Brown <michaelbrown4343@gmail.com>
2025-10-19 06:17:24 +1100

    fix build

--- app/(marketing)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UploadDropzone } from "@/components/upload-dropzone";
-
-export default function LandingPage() {
-  return (
-    <div className="flex min-h-[calc(100vh-6rem)] flex-col justify-center pb-16">
-      <UploadDropzone />
-    </div>
-  );
-}
